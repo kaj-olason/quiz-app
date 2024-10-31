c46c01869320dffd66234edb4ea098cdee2f47d8
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-10-31 14:57:18 +0100

    First Draft

---
+++ js/index.js
@@ -0,0 +1,56 @@
+
+//let quizObject = readJsonFile("./json/quiz.json");
+
+let quizObject = {
+    quizArray : [ 
+        {
+            quizzName : "Djur Quiz",
+            questions : [
+                {
+                    question : "Hur många ben har en myra?",
+                    answers : ["4st", "6st", "8st", "10st"],
+                    correctIndexAnswer : 1
+                },
+                {
+                    question : "En zebra är",
+                    answers : ["rutig", "grön", "randig", "blå"],
+                    correctIndexAnswer : 2
+                }
+            ]
+        },
+        {
+            quizzName : "Data Quiz",
+            questions : [
+                {
+                    question : "Vilken av följande är ett programmeringsspråk?",
+                    answers : ["Hamburger", "Coffe", "Whiskey", "Java"],
+                    correctIndexAnswer : 3
+                },
+                {
+                    question : "Vad står MB för",
+                    answers : ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
+                    correctIndexAnswer : 0
+                }
+            ]
+        }
+    ]
+};
+
+let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
+let currentAnswers = [];
+
+
+    /* Baker */
+
+
+
+    /* Henrik *
+
+
+
+    /* Kaj */
+
+
+    
+    /* Viktor */
+
