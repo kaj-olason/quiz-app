89c0bc68dd9e1ad274ef78eefea49d0b48cbc146
Chas-Henrik <henrik.suurik@chasacademy.se>
2024-11-01 09:01:38 +0100

    Feature/skeleton project (#9)
    * designskiss completed
    
    * designskiss completed
    
    * designskiss completed
    
    * First Draft
    
    * Added html stubs
    
    * Cleanup of quizObject
    
    * Changed stub id's to unique names
    
    * Added missing index.css link
    
    * Added debugger
    
    * Minor cleanup
    
    * Added missing  const
    
    ---------
    
    Co-authored-by: by021 <baker.yousif@live.se>

---
+++ js/index.js
@@ -0,0 +1,61 @@
+debugger;
+
+const quizSelectElement = document.getElementById("quiz-select");
+const quizRunElement = document.getElementById("quiz-run");
+const quizResultElement = document.getElementById("quiz-result");
+
+//let quizObject = await readJsonFile("./json/quiz.json");
+
+let quizObject = {
+    quizArray : [ 
+        {
+            quizName : "Djur Quiz",
+            questionsArray : [
+                {
+                    question : "Hur många ben har en myra?",
+                    answers : ["4st", "6st", "8st", "10st"],
+                    correctIndexAnswer : 1
+                },
+                {
+                    question : "En zebra är...",
+                    answers : ["rutig", "grön", "randig", "blå"],
+                    correctIndexAnswer : 2
+                }
+            ]
+        },
+        {
+            quizName : "Data Quiz",
+            questionsArray : [
+                {
+                    question : "Vilken av följande är ett programmeringsspråk?",
+                    answers : ["Hamburger", "Coffee", "Whiskey", "Java"],
+                    correctIndexAnswer : 3
+                },
+                {
+                    question : "Vad står MB för?",
+                    answers : ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
+                    correctIndexAnswer : 0
+                }
+            ]
+        }
+    ]
+};
+
+let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
+let currentAnswers = [];
+
+
+/* Baker */
+
+
+
+/* Henrik *
+
+
+
+/* Kaj */
+
+
+    
+/* Viktor */
+
