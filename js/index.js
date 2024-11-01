4c8412f200b11f73014c96004cadea7d5afdd9c5
kaj-olason <kaj.olason@icloud.com>
2024-11-01 16:37:49 +0100

    Created json-file and saved it in local storage

--- js/index.js
+++ js/index.js
@@ -1,53 +1,59 @@
-debugger;
+// Import the quiz-object from json-file
+import quizDataJson from "../quizdata.json" with { type: "json" };
+
+// Store the quiz-object into local-storage
+localStorage.setItem("quizData", JSON.stringify(quizDataJson));
+
+const quizData = JSON.parse(localStorage.getItem("quizData"));
+
 
 const quizSelectElement = document.getElementById("quiz-select");
 const quizRunElement = document.getElementById("quiz-run");
 const quizResultElement = document.getElementById("quiz-result");
 
-//let quizObject = await readJsonFile("./json/quiz.json");
+
+
+console.log(quizData);
 
 let quizObject = {
-    quizArray : [ 
+  quizArray: [
+    {
+      quizName: "Djur Quiz",
+      questionsArray: [
         {
-            quizName : "Djur Quiz",
-            questionsArray : [
-                {
-                    question : "Hur många ben har en myra?",
-                    answers : ["4st", "6st", "8st", "10st"],
-                    correctIndexAnswer : 1
-                },
-                {
-                    question : "En zebra är...",
-                    answers : ["rutig", "grön", "randig", "blå"],
-                    correctIndexAnswer : 2
-                }
-            ]
+          question: "Hur många ben har en myra?",
+          answers: ["4st", "6st", "8st", "10st"],
+          correctIndexAnswer: 1,
         },
         {
-            quizName : "Data Quiz",
-            questionsArray : [
-                {
-                    question : "Vilken av följande är ett programmeringsspråk?",
-                    answers : ["Hamburger", "Coffee", "Whiskey", "Java"],
-                    correctIndexAnswer : 3
-                },
-                {
-                    question : "Vad står MB för?",
-                    answers : ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
-                    correctIndexAnswer : 0
-                }
-            ]
-        }
-    ]
+          question: "En zebra är...",
+          answers: ["rutig", "grön", "randig", "blå"],
+          correctIndexAnswer: 2,
+        },
+      ],
+    },
+    {
+      quizName: "Data Quiz",
+      questionsArray: [
+        {
+          question: "Vilken av följande är ett programmeringsspråk?",
+          answers: ["Hamburger", "Coffee", "Whiskey", "Java"],
+          correctIndexAnswer: 3,
+        },
+        {
+          question: "Vad står MB för?",
+          answers: ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
+          correctIndexAnswer: 0,
+        },
+      ],
+    },
+  ],
 };
 
 let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
 let currentAnswers = [];
 
-
 /* Baker */
-
-
 
 /* Henrik *
 
@@ -55,7 +61,4 @@ let currentAnswers = [];
 
 /* Kaj */
 
-
-    
 /* Viktor */
-
