a9a0c7bbac24aa19f3f339d26c9eb6511ae31640
Viktor Gustafsson <vigus006@gmail.com>
2024-11-03 10:35:16 +0100

    format files with prettier

--- js/index.js
+++ js/index.js
@@ -7,49 +7,46 @@ const quizResultElement = document.getElementById("quiz-result");
 //let quizObject = await readJsonFile("./json/quiz.json");
 
 let quizObject = {
-    quizArray : [ 
+  quizArray: [
+    {
+      quizName: "Djur Quiz",
+      timeLimitSec: 30,
+      questionsArray: [
         {
-            quizName : "Djur Quiz",
-            timeLimitSec : 30,
-            questionsArray : [
-                {
-                    question : "Hur många ben har en myra?",
-                    answers : ["4st", "6st", "8st", "10st"],
-                    correctIndexAnswer : 1
-                },
-                {
-                    question : "En zebra är...",
-                    answers : ["rutig", "grön", "randig", "blå"],
-                    correctIndexAnswer : 2
-                }
-            ]
+          question: "Hur många ben har en myra?",
+          answers: ["4st", "6st", "8st", "10st"],
+          correctIndexAnswer: 1,
         },
         {
-            quizName : "Data Quiz",
-            timeLimitSec : 30,
-            questionsArray : [
-                {
-                    question : "Vilken av följande är ett programmeringsspråk?",
-                    answers : ["Hamburger", "Coffee", "Whiskey", "Java"],
-                    correctIndexAnswer : 3
-                },
-                {
-                    question : "Vad står MB för?",
-                    answers : ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
-                    correctIndexAnswer : 0
-                }
-            ]
-        }
-    ]
+          question: "En zebra är...",
+          answers: ["rutig", "grön", "randig", "blå"],
+          correctIndexAnswer: 2,
+        },
+      ],
+    },
+    {
+      quizName: "Data Quiz",
+      timeLimitSec: 30,
+      questionsArray: [
+        {
+          question: "Vilken av följande är ett programmeringsspråk?",
+          answers: ["Hamburger", "Coffee", "Whiskey", "Java"],
+          correctIndexAnswer: 3,
+        },
+        {
+          question: "Vad står MB för?",
+          answers: ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
+          correctIndexAnswer: 0,
+        },
+      ],
+    },
+  ],
 };
 
 let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
 let currentAnswers = [];
 
-
 /* Baker */
-
-
 
 /* Henrik *
 
@@ -57,8 +54,6 @@ let currentAnswers = [];
 
 /* Kaj */
 
-
-    
 /* Viktor */
 
 // const quizTimer = document.getElementById("quizTimer");
@@ -72,41 +67,36 @@ let timerCountSec = 0;
 let timerInterval;
 
 function startTimer() {
-    
-    // A Condition which prevents a new interval to be started if an interval is already ongoing
-    if (!timerInterval) {
-        timerInterval = setInterval(updateTimer, 1000);    
-    }
+  // A Condition which prevents a new interval to be started if an interval is already ongoing
+  if (!timerInterval) {
+    timerInterval = setInterval(updateTimer, 1000);
+  }
 }
 
 function updateTimer() {
+  timerCountSec++;
 
-    timerCountSec++;
-    
-    //Made a condition to make timer start at 00 instead of 0
-    if (timerCountSec < 10) {
-        quizTimer.innerHTML = "0" + timerCountSec;
-    } else {
-        quizTimer.innerHTML = timerCountSec;
-    }      
-    
-    if (timerCountSec === currentQuiz.timeLimitSec) {
-        pauseTimer()
-    }
+  //Made a condition to make timer start at 00 instead of 0
+  if (timerCountSec < 10) {
+    quizTimer.innerHTML = "0" + timerCountSec;
+  } else {
+    quizTimer.innerHTML = timerCountSec;
+  }
+
+  if (timerCountSec === currentQuiz.timeLimitSec) {
+    pauseTimer();
+  }
 }
 
 function pauseTimer() {
-    
-    clearInterval(timerInterval);
-    timerInterval = null; 
+  clearInterval(timerInterval);
+  timerInterval = null;
 }
 
 function resetTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null;
 
-    clearInterval(timerInterval);
-    timerInterval = null;
-    
-    timerCountSec = 0;
-    quizTimer.innerHTML = "0" + timerCountSec;
+  timerCountSec = 0;
+  quizTimer.innerHTML = "0" + timerCountSec;
 }
-
