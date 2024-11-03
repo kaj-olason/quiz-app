193c92a835aced1023561391ac07d916914b4b1e
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-11-03 15:59:09 +0100

    Added quiz slider

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ if (localStorage.getItem("quizObject")) {
   localStorage.setItem("quizObject", JSON.stringify(quizObject));
 }
 
-const quizSelectElement = document.getElementById("quiz-select");
+const quizSelectElement = document.getElementById("quiz-select-id");
 const quizRunElement = document.getElementById("quiz-run");
 const quizResultElement = document.getElementById("quiz-result");
 
@@ -24,18 +24,26 @@ let currentAnswers = [];
 
 /* Henrik */
 
-loadQuizzes();
+const quizListElement = document.getElementById("quiz-list-id");
+const quizSliderElement = document.getElementById("quiz-slider-id");
+quizListElement.classList.add("collapsed");
+quizSliderElement.classList.add("collapsed");
 
-function loadQuizzes() {
+/* Load function */
+
+loadQuizzes(quizListElement, "quiz-list-item");
+loadQuizzes(quizSliderElement, "quiz-slider-item");
+mediaQueryEventHandler();
+
+function loadQuizzes(elementObj, classNames) {
     for(let i=0; i<quizObject.quizArray.length; i++) {
         const quiz = quizObject.quizArray[i];
-        const quizName = quiz.quizName;
         const quizElement = document.createElement("p");
         quizElement.dataset.id = i;
-        quizElement.classList.add("quiz-select-item");
+        quizElement.classList.add(classNames);
         quizElement.addEventListener("click", (e) => selectQuiz(quizElement));
         quizElement.innerHTML = quiz.quizName;
-        quizSelectElement.appendChild(quizElement);
+        elementObj.appendChild(quizElement);
     }
 }
 
@@ -44,10 +52,67 @@ function selectQuiz(quiz) {
     quizSelectElement.classList.add("collapsed");
     quizRunElement.classList.remove("collapsed");
     
+    console.log(`Quiz ${quiz.dataset.id} selected`);
     // loadQuiz();
 }
 
+/* Media query handler */
 
+const mediaQueryList = window.matchMedia("(min-width: 1024px)");
+
+mediaQueryList.addEventListener("change", (event) => mediaQueryEventHandler());
+
+function mediaQueryEventHandler() {
+    const width = parseInt(window.getComputedStyle(document.querySelector("body")).getPropertyValue('width'));
+
+    if(width >= 1024) {
+        quizListElement.classList.add("collapsed");
+        quizSliderElement.classList.remove("collapsed");
+    } else {
+        quizListElement.classList.remove("collapsed");
+        quizSliderElement.classList.add("collapsed");
+    }
+}
+
+/* Scroll event handler */
+
+quizSliderElement.addEventListener("scroll", (e) => scrollEventHandler(e));
+
+function scrollEventHandler(e) {
+    console.log("scroll event");
+    const quizSliderItemsElement = Array.from(document.getElementsByClassName("quiz-slider-item"));
+    const quizSliderItemsElementWidth = parseInt(window.getComputedStyle(quizSliderItemsElement[0]).getPropertyValue('width'));
+    const scrollPos = quizSliderElement.scrollLeft / quizSliderItemsElementWidth;
+    scaleSliderItems(quizSliderItemsElement, scrollPos);
+}
+
+function scaleSliderItems(quizSliderItemsElement, scrollPos) {
+    scrollPos = Math.max(0, Math.min(scrollPos, quizSliderItemsElement.length-1));
+    console.log("scrollPos", scrollPos);
+    const indexInFocus = Math.round(scrollPos);
+    const indexLeft = Math.floor(scrollPos);
+    const indexRight = Math.ceil(scrollPos);
+    const indexLeftDiff = scrollPos - indexLeft;
+    const indexRightDiff = indexRight - scrollPos;
+    const leftFontSize = 1 + 8*(1-indexLeftDiff);
+    const rightFontSize = 1 + 8*(1-indexRightDiff);
+
+    for(let i=0; i<=quizSliderItemsElement.length-1; i++) {
+        quizSliderItemsElement[i].style.fontSize = `1rem`;
+        quizSliderItemsElement[i].style.color = "#000000";
+        quizSliderItemsElement[i].style.zIndex = "0";
+        quizSliderItemsElement[i].style.cursor = "default";
+        quizSliderItemsElement[i].removeEventListener("click", (e) => SliderQuiz(e.target));
+    }
+
+    quizSliderItemsElement[indexLeft].style.fontSize = `${leftFontSize}rem`;
+    quizSliderItemsElement[indexRight].style.fontSize = `${rightFontSize}rem`;
+
+    quizSliderItemsElement[indexInFocus].style.cursor = "pointer";
+    quizSliderItemsElement[indexInFocus].style.color = "#54C4F8";
+    quizSliderItemsElement[indexInFocus].style.zIndex = "1";
+    quizSliderItemsElement[indexInFocus].addEventListener("click", (e) => selectQuiz(e.target));
+}
 
 
 /* Kaj */
