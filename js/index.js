595097d66766c279c2615297578719e5869b7c0f
Viktor Gustafsson <vigus006@gmail.com>
2024-11-03 15:04:32 +0100

    cleaning index.js before merge

--- js/index.js
+++ js/index.js
@@ -1,47 +1,6 @@
-debugger;
-
 const quizSelectElement = document.getElementById("quiz-select");
 const quizRunElement = document.getElementById("quiz-run");
 const quizResultElement = document.getElementById("quiz-result");
-
-//let quizObject = await readJsonFile("./json/quiz.json");
-
-let quizObject = {
-  quizArray: [
-    {
-      quizName: "Djur Quiz",
-      timeLimitSec: 30,
-      questionsArray: [
-        {
-          question: "Hur många ben har en myra?",
-          answers: ["4st", "6st", "8st", "10st"],
-          correctIndexAnswer: 1,
-        },
-        {
-          question: "En zebra är...",
-          answers: ["rutig", "grön", "randig", "blå"],
-          correctIndexAnswer: 2,
-        },
-      ],
-    },
-    {
-      quizName: "Data Quiz",
-      timeLimitSec: 30,
-      questionsArray: [
-        {
-          question: "Vilken av följande är ett programmeringsspråk?",
-          answers: ["Hamburger", "Coffee", "Whiskey", "Java"],
-          correctIndexAnswer: 3,
-        },
-        {
-          question: "Vad står MB för?",
-          answers: ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
-          correctIndexAnswer: 0,
-        },
-      ],
-    },
-  ],
-};
 
 let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
 let currentAnswers = [];
