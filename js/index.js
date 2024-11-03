cd1bc7b058db243cb8427055a94d80ff95fe9b26
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-11-03 16:01:10 +0100

    Pretty formatted html, css & js

--- js/index.js
+++ js/index.js
@@ -15,12 +15,51 @@ const quizSelectElement = document.getElementById("quiz-select-id");
 const quizRunElement = document.getElementById("quiz-run");
 const quizResultElement = document.getElementById("quiz-result");
 
+<<<<<<< HEAD
 let currentQuiz = quizObject[0]; // 0 = Djur Quiz, 1 = Data Quiz
+=======
+//let quizObject = await readJsonFile("./json/quiz.json");
+
+let quizObject = {
+  quizArray: [
+    {
+      quizName: "Djur Quiz",
+      questionsArray: [
+        {
+          question: "Hur många ben har en myra?",
+          answers: ["4st", "6st", "8st", "10st"],
+          correctIndexAnswer: 1,
+        },
+        {
+          question: "En zebra är...",
+          answers: ["rutig", "grön", "randig", "blå"],
+          correctIndexAnswer: 2,
+        },
+      ],
+    },
+    {
+      quizName: "Data Quiz",
+      questionsArray: [
+        {
+          question: "Vilken av följande är ett programmeringsspråk?",
+          answers: ["Hamburger", "Coffee", "Whiskey", "Java"],
+          correctIndexAnswer: 3,
+        },
+        {
+          question: "Vad står MB för?",
+          answers: ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
+          correctIndexAnswer: 0,
+        },
+      ],
+    },
+  ],
+};
+
+let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
+>>>>>>> 9094571 (Pretty formatted html, css & js)
 let currentAnswers = [];
 
 /* Baker */
-
-
 
 /* Henrik */
 
@@ -36,24 +75,24 @@ loadQuizzes(quizSliderElement, "quiz-slider-item");
 mediaQueryEventHandler();
 
 function loadQuizzes(elementObj, classNames) {
-    for(let i=0; i<quizObject.quizArray.length; i++) {
-        const quiz = quizObject.quizArray[i];
-        const quizElement = document.createElement("p");
-        quizElement.dataset.id = i;
-        quizElement.classList.add(classNames);
-        quizElement.addEventListener("click", (e) => selectQuiz(quizElement));
-        quizElement.innerHTML = quiz.quizName;
-        elementObj.appendChild(quizElement);
-    }
+  for (let i = 0; i < quizObject.quizArray.length; i++) {
+    const quiz = quizObject.quizArray[i];
+    const quizElement = document.createElement("p");
+    quizElement.dataset.id = i;
+    quizElement.classList.add(classNames);
+    quizElement.addEventListener("click", (e) => selectQuiz(quizElement));
+    quizElement.innerHTML = quiz.quizName;
+    elementObj.appendChild(quizElement);
+  }
 }
 
 function selectQuiz(quiz) {
-    currentQuiz = quizObject.quizArray[quiz.dataset.id];
-    quizSelectElement.classList.add("collapsed");
-    quizRunElement.classList.remove("collapsed");
-    
-    console.log(`Quiz ${quiz.dataset.id} selected`);
-    // loadQuiz();
+  currentQuiz = quizObject.quizArray[quiz.dataset.id];
+  quizSelectElement.classList.add("collapsed");
+  quizRunElement.classList.remove("collapsed");
+
+  console.log(`Quiz ${quiz.dataset.id} selected`);
+  // loadQuiz();
 }
 
 /* Media query handler */
@@ -63,15 +102,19 @@ const mediaQueryList = window.matchMedia("(min-width: 1024px)");
 mediaQueryList.addEventListener("change", (event) => mediaQueryEventHandler());
 
 function mediaQueryEventHandler() {
-    const width = parseInt(window.getComputedStyle(document.querySelector("body")).getPropertyValue('width'));
+  const width = parseInt(
+    window
+      .getComputedStyle(document.querySelector("body"))
+      .getPropertyValue("width")
+  );
 
-    if(width >= 1024) {
-        quizListElement.classList.add("collapsed");
-        quizSliderElement.classList.remove("collapsed");
-    } else {
-        quizListElement.classList.remove("collapsed");
-        quizSliderElement.classList.add("collapsed");
-    }
+  if (width >= 1024) {
+    quizListElement.classList.add("collapsed");
+    quizSliderElement.classList.remove("collapsed");
+  } else {
+    quizListElement.classList.remove("collapsed");
+    quizSliderElement.classList.add("collapsed");
+  }
 }
 
 /* Scroll event handler */
@@ -79,41 +122,51 @@ function mediaQueryEventHandler() {
 quizSliderElement.addEventListener("scroll", (e) => scrollEventHandler(e));
 
 function scrollEventHandler(e) {
-    console.log("scroll event");
-    const quizSliderItemsElement = Array.from(document.getElementsByClassName("quiz-slider-item"));
-    const quizSliderItemsElementWidth = parseInt(window.getComputedStyle(quizSliderItemsElement[0]).getPropertyValue('width'));
-    const scrollPos = quizSliderElement.scrollLeft / quizSliderItemsElementWidth;
-    scaleSliderItems(quizSliderItemsElement, scrollPos);
+  console.log("scroll event");
+  const quizSliderItemsElement = Array.from(
+    document.getElementsByClassName("quiz-slider-item")
+  );
+  const quizSliderItemsElementWidth = parseInt(
+    window.getComputedStyle(quizSliderItemsElement[0]).getPropertyValue("width")
+  );
+  const scrollPos = quizSliderElement.scrollLeft / quizSliderItemsElementWidth;
+  scaleSliderItems(quizSliderItemsElement, scrollPos);
 }
 
 function scaleSliderItems(quizSliderItemsElement, scrollPos) {
-    scrollPos = Math.max(0, Math.min(scrollPos, quizSliderItemsElement.length-1));
-    console.log("scrollPos", scrollPos);
-    const indexInFocus = Math.round(scrollPos);
-    const indexLeft = Math.floor(scrollPos);
-    const indexRight = Math.ceil(scrollPos);
-    const indexLeftDiff = scrollPos - indexLeft;
-    const indexRightDiff = indexRight - scrollPos;
-    const leftFontSize = 1 + 8*(1-indexLeftDiff);
-    const rightFontSize = 1 + 8*(1-indexRightDiff);
+  scrollPos = Math.max(
+    0,
+    Math.min(scrollPos, quizSliderItemsElement.length - 1)
+  );
+  console.log("scrollPos", scrollPos);
+  const indexInFocus = Math.round(scrollPos);
+  const indexLeft = Math.floor(scrollPos);
+  const indexRight = Math.ceil(scrollPos);
+  const indexLeftDiff = scrollPos - indexLeft;
+  const indexRightDiff = indexRight - scrollPos;
+  const leftFontSize = 1 + 8 * (1 - indexLeftDiff);
+  const rightFontSize = 1 + 8 * (1 - indexRightDiff);
 
-    for(let i=0; i<=quizSliderItemsElement.length-1; i++) {
-        quizSliderItemsElement[i].style.fontSize = `1rem`;
-        quizSliderItemsElement[i].style.color = "#000000";
-        quizSliderItemsElement[i].style.zIndex = "0";
-        quizSliderItemsElement[i].style.cursor = "default";
-        quizSliderItemsElement[i].removeEventListener("click", (e) => SliderQuiz(e.target));
-    }
+  for (let i = 0; i <= quizSliderItemsElement.length - 1; i++) {
+    quizSliderItemsElement[i].style.fontSize = `1rem`;
+    quizSliderItemsElement[i].style.color = "#000000";
+    quizSliderItemsElement[i].style.zIndex = "0";
+    quizSliderItemsElement[i].style.cursor = "default";
+    quizSliderItemsElement[i].removeEventListener("click", (e) =>
+      SliderQuiz(e.target)
+    );
+  }
 
-    quizSliderItemsElement[indexLeft].style.fontSize = `${leftFontSize}rem`;
-    quizSliderItemsElement[indexRight].style.fontSize = `${rightFontSize}rem`;
+  quizSliderItemsElement[indexLeft].style.fontSize = `${leftFontSize}rem`;
+  quizSliderItemsElement[indexRight].style.fontSize = `${rightFontSize}rem`;
 
-    quizSliderItemsElement[indexInFocus].style.cursor = "pointer";
-    quizSliderItemsElement[indexInFocus].style.color = "#54C4F8";
-    quizSliderItemsElement[indexInFocus].style.zIndex = "1";
-    quizSliderItemsElement[indexInFocus].addEventListener("click", (e) => selectQuiz(e.target));
+  quizSliderItemsElement[indexInFocus].style.cursor = "pointer";
+  quizSliderItemsElement[indexInFocus].style.color = "#54C4F8";
+  quizSliderItemsElement[indexInFocus].style.zIndex = "1";
+  quizSliderItemsElement[indexInFocus].addEventListener("click", (e) =>
+    selectQuiz(e.target)
+  );
 }
-
 
 /* Kaj */
 
