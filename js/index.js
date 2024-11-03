47e3ecf2d1f25145298aa494b6c72fde82486455
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-11-03 16:16:22 +0100

    Resolved merge conflicts

--- js/index.js
+++ js/index.js
@@ -15,48 +15,7 @@ const quizSelectElement = document.getElementById("quiz-select-id");
 const quizRunElement = document.getElementById("quiz-run");
 const quizResultElement = document.getElementById("quiz-result");
 
-<<<<<<< HEAD
 let currentQuiz = quizObject[0]; // 0 = Djur Quiz, 1 = Data Quiz
-=======
-//let quizObject = await readJsonFile("./json/quiz.json");
-
-let quizObject = {
-  quizArray: [
-    {
-      quizName: "Djur Quiz",
-      questionsArray: [
-        {
-          question: "Hur många ben har en myra?",
-          answers: ["4st", "6st", "8st", "10st"],
-          correctIndexAnswer: 1,
-        },
-        {
-          question: "En zebra är...",
-          answers: ["rutig", "grön", "randig", "blå"],
-          correctIndexAnswer: 2,
-        },
-      ],
-    },
-    {
-      quizName: "Data Quiz",
-      questionsArray: [
-        {
-          question: "Vilken av följande är ett programmeringsspråk?",
-          answers: ["Hamburger", "Coffee", "Whiskey", "Java"],
-          correctIndexAnswer: 3,
-        },
-        {
-          question: "Vad står MB för?",
-          answers: ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
-          correctIndexAnswer: 0,
-        },
-      ],
-    },
-  ],
-};
-
-let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
->>>>>>> 9094571 (Pretty formatted html, css & js)
 let currentAnswers = [];
 
 /* Baker */
