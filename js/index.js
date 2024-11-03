4e7a1ca05ce0899e5ae3e3a669eb1669f462c932
kaj-olason <kaj.olason@icloud.com>
2024-11-03 12:08:48 +0100

    Include statement for localstorage

--- js/index.js
+++ js/index.js
@@ -1,56 +1,21 @@
-import quizDataJson from "../quizdata.json" with { type: "json" };
+let quizObject = {};
 
-let quizData = {};
-
-if (localStorage.getItem("quizData")) {
-  quizData = JSON.parse(localStorage.getItem("quizData"));
+if (localStorage.getItem("quizObject")) {
+  quizObject = JSON.parse(localStorage.getItem("quizObject"));
 } else {
+  // Fetsh data from json-file if data not exist in local storage
+  const response = await fetch("../quizdata.json");
+  quizObject = await response.json();
+
   // Store the quiz-object into local-storage
-  localStorage.setItem("quizData", JSON.stringify(quizDataJson));
+  localStorage.setItem("quizObject", JSON.stringify(quizObject));
 }
 
 const quizSelectElement = document.getElementById("quiz-select");
 const quizRunElement = document.getElementById("quiz-run");
 const quizResultElement = document.getElementById("quiz-result");
 
-console.log(quizData);
-
-let quizObject = {
-  quizArray: [
-    {
-      quizName: "Djur Quiz",
-      questionsArray: [
-        {
-          question: "Hur många ben har en myra?",
-          answers: ["4st", "6st", "8st", "10st"],
-          correctIndexAnswer: 1,
-        },
-        {
-          question: "En zebra är...",
-          answers: ["rutig", "grön", "randig", "blå"],
-          correctIndexAnswer: 2,
-        },
-      ],
-    },
-    {
-      quizName: "Data Quiz",
-      questionsArray: [
-        {
-          question: "Vilken av följande är ett programmeringsspråk?",
-          answers: ["Hamburger", "Coffee", "Whiskey", "Java"],
-          correctIndexAnswer: 3,
-        },
-        {
-          question: "Vad står MB för?",
-          answers: ["Mera Byte", "Många Byte", "Mega Byte", "Multi Byte"],
-          correctIndexAnswer: 0,
-        },
-      ],
-    },
-  ],
-};
-
-let currentQuiz = quizObject.quizArray[0]; // 0 = Djur Quiz, 1 = Data Quiz
+let currentQuiz = quizObject[0]; // 0 = Djur Quiz, 1 = Data Quiz
 let currentAnswers = [];
 
 /* Baker */
